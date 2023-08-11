de55e7a83870eb19fb0ee01a77098ddc9ff17a7b
lyyy <lyyy.prc@gmail.com>
2023-08-11 15:52:38 +0800

    2023 0811

---
+++ code/about_type/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// 自定义类型和类型别名示意
+
+// 1.自定义类型
+
+// Myint 基于int类型的自定义
+type Myint int
+
+
+//2.类型别名
+
+// Newint int类型别名
+type Newint = int
+
+
+func main(){
+var i Myint
+fmt.Printf("type:%T value%v\n", i,i)
+
+var x Newint
+fmt.Printf("type:%T value%v\n",x,x)
+}
