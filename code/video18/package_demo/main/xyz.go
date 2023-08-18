754dff79ae96d17fa9cc21bbc0f164cad2c91fc5
lyyy <lyyy.prc@gmail.com>
2023-08-18 19:50:44 +0800

    2023 0818

---
+++ code/video18/package_demo/main/xyz.go
@@ -0,0 +1,14 @@
+package main//略过
+
+// import "fmt"
+
+
+// 当你的代码都放在$GOPATH目录下的话
+// 我们导入包的路径要从$GOPATH/src后面写起
+
+// func main() {
+// 	fmt.Println("hello")
+// 	ret := calc.Add(10, 20)
+// 	fmt.Println(ret)
+// 	fmt.Println(calc, Name)
+// }
